01d084bcc797c29d5cd1353132eb704dcec5b174
AR Associates <ar.associates1957@gmail.com>
2025-12-04 10:19:18 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import {
 } from 'lucide-react';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const generateRoomCode = () => {
